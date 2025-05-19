5ff3156989912e6def5d5fad0dc1b8fd586fd8f6
kushalraga <kushalraga20@gmail.com>
2025-05-19 17:54:18 +0530

    added single blog page

--- blog/blog_posts/views.py
+++ blog/blog_posts/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import BlogPost
+from django.http import HttpResponse
 
 # Create your views here.
 def blog_home(request):
     posts = BlogPost.objects.all()
     return render(request, 'blog_posts/blog_home.html', {'posts': posts})
+
+def single_post_page(request, post_id):
+    single_post = get_object_or_404(BlogPost, id=post_id)
+    return render(request, 'blog_posts/single_post.html', {'single_post': single_post})
